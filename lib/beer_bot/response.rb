4a211233d8a45f092ecde4459fb82958e05bb386
Charles Treatman <charles.treatman@gmail.com>
2015-01-19 13:19:14 -0500

    Handles messaging when search result size is <= 1

--- lib/beer_bot/response.rb
+++ lib/beer_bot/response.rb
@@ -1,31 +1,50 @@
 module BeerBot
   class Response
     def initialize(bars, request_params)
-      @response_fields = {
-        link_names: 1,
-        username: 'BeerBot',
-        icon_emoji: ':beers:',
-        channel: request_params['channel_id'],
-        text: build_response_text(bars, request_params)
-      }
+      @bars = bars
+      @request_params = request_params
     end
 
     def to_json
-      @response_fields.to_json
+      {
+        link_names: 1,
+        username: 'BeerBot',
+        icon_emoji: ':beers:',
+        channel: @request_params['channel_id'],
+        text: build_response_text
+      }.to_json
     end
 
   private
 
-    def build_response_text(bars, request_params)
+    def build_response_text
       lines = []
-      lines << "@#{request_params['user_name']}: I found #{bars.size} bars for '#{request_params['text']}':"
-      bars.each do |bar|
+      lines << "@#{@request_params['user_name']}: I #{found} #{bar_or_bars} for '#{@request_params['text']}'."
+      lines += build_tap_lists
+      lines.join("\n")
+    end
+
+    def found
+      case @bars.size
+      when 0
+        found = 'didn\'t find any'
+      else
+        found = "found #{@bars.size}"
+      end
+    end
+
+    def bar_or_bars
+      @bars.size == 1 ? 'bar' : 'bars'
+    end
+
+    def build_tap_lists
+      @bars.inject([]) do |lines, bar|
         lines << "1. #{bar.name} has #{bar.beers.size} beers on tap"
         bar.beers.each do |beer|
           lines << "    * #{beer[:name]} (#{beer[:style]}, #{beer[:origin]})"
         end
+        lines
       end
-      lines.join("\n")
     end
   end
 end
