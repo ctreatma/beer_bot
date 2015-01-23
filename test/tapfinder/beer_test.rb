15b9d3ed7bf6b1eef2a6a72357584213e3450e1f
Charles Treatman <charles.treatman@gmail.com>
2015-01-23 12:00:56 -0500

    Fixes #7: Don't squash listings when a beer is at multiple bars
    Bug was introduced by the fix for #5

--- test/tapfinder/beer_test.rb
+++ test/tapfinder/beer_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 describe 'A beer that is available at bars and events' do
   before do
-    VCR.use_cassette('rueuze') do
-      @beer = Tapfinder::Beer.load( [ { 'link' => '/?p=41300' } ] ).first
+    VCR.use_cassette('supplication') do
+      @beer = Tapfinder::Beer.load( [ { 'link' => '/beer/russian-river-supplication/' } ] ).first
     end
   end
 
   it 'should have the correct name' do
-    @beer.name.must_equal 'Bruery Rueuze'
+    @beer.name.must_equal 'Russian River Supplication'
   end
 
   it 'should have the correct style' do
@@ -16,25 +16,26 @@ describe 'A beer that is available at bars and events' do
   end
 
   it 'should have the correct origin' do
-    @beer.origin.must_equal 'Placentia, CA'
+    @beer.origin.must_equal 'Santa Rosa, CA'
   end
 
   it 'should have the correct bar list' do
     @beer.bars.must_equal [
-      { name: 'Pinocchio\'s', address: '131 E. Baltimore Ave., Media, PA', updated_at: '01/20/15' }
+      { name: 'Good Dog', address: '224 South 15th St. (between Walnut &...', updated_at: '01/22/15' },
+      { name: 'Barcade', address: '1114 Frankford Ave.', updated_at: '01/22/15' }
     ]
   end
 
   it 'should have the correct event list' do
     @beer.events.must_equal [
-      { name: 'Sour Bowl', bar: 'Brü Craft & Wurst', address: '1316 Chestnut St.', date: '01/25/2015' }
+      { name: 'Russian River', bar: 'Isaac Newton\'s', address: '18 South State St., ...', date: '01/31/2015' }
     ]
   end
 end
 
 describe 'A beer that is available only at events' do
   before do
-    VCR.use_cassette('cantillon') do
+    VCR.use_cassette('consecration') do
       @beer = Tapfinder::Beer.load( [ { 'link' => '/?p=35601' } ] ).first
     end
   end
