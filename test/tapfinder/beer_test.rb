10fed631cf2ebd7309184ac7f01f28c0c61bf756
Charles Treatman <charles.treatman@gmail.com>
2015-01-23 10:44:27 -0500

    Fixes #5: Don't display events twice when a beer is at 0 bars
    The PTF beer page structure doesn't allow for a selector that
    will only fetch bar listings, so the bar selector will match
    event listings when no bars are present.
    
    This updates the `Tapfinder::Beer#bars` method to return an
    empty array if the matched element contains a '.events-panel'
    element.

--- test/tapfinder/beer_test.rb
+++ test/tapfinder/beer_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-describe Tapfinder::Beer do
+describe 'A beer that is available at bars and events' do
   before do
     VCR.use_cassette('rueuze') do
       @beer = Tapfinder::Beer.load( [ { 'link' => '/?p=41300' } ] ).first
@@ -27,6 +27,37 @@ describe Tapfinder::Beer do
 
   it 'should have the correct event list' do
     @beer.events.must_equal [
+      { name: 'Sour Bowl', bar: 'Brü Craft & Wurst', address: '1316 Chestnut St.', date: '01/25/2015' }
+    ]
+  end
+end
+
+describe 'A beer that is available only at events' do
+  before do
+    VCR.use_cassette('cantillon') do
+      @beer = Tapfinder::Beer.load( [ { 'link' => '/?p=35601' } ] ).first
+    end
+  end
+
+  it 'should have the correct name' do
+    @beer.name.must_equal 'Russian River Consecration'
+  end
+
+  it 'should have the correct style' do
+    @beer.style.must_equal 'Sour/Wild-Fermented Ale'
+  end
+
+  it 'should have the correct origin' do
+    @beer.origin.must_equal 'Santa Rosa, CA'
+  end
+
+  it 'should have the correct bar list' do
+    @beer.bars.must_equal []
+  end
+
+  it 'should have the correct event list' do
+    @beer.events.must_equal [
+      { name: 'Russian River', bar: 'Isaac Newton\'s', address: '18 South State St., ...', date: '01/31/2015' },
       { name: 'Sour Bowl', bar: 'Brü Craft & Wurst', address: '1316 Chestnut St.', date: '01/25/2015' }
     ]
   end
